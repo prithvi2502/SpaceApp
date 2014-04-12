7e8f313844430a5b3f63174bd858a6566e3f708a
prithvi2502 <prithvi2502@gmail.com>
2014-04-11 22:01:03 -0400

    First Draft
    This is just the first draft

---
+++ SpaceEmberApp/src/com/example/spaceemberapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.spaceemberapp;
+
+
+
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button btnLaunch = (Button) findViewById(R.id.button1);
+		btnLaunch.setOnClickListener(new OnClickListener(){
+			
+			public void onClick(View v){
+				Intent intent = new Intent(v.getContext(), StatusActivity.class);
+				startActivityForResult(intent, 0);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
